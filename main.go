eedef2283e11fc22bf05a2602281299a12656a0e
Guido Gonzalez <guidomarianogonzalez@hotmail.com>
2024-01-21 14:47:54 -0300

    tercer commit

--- main.go
+++ main.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, texto := variables.ConviertoTexto(1588)
+	/*estado, texto := variables.ConviertoTexto(1588)
 	fmt.Println(estado)
-	fmt.Println(texto)
+	fmt.Println(texto)*/
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es Windows, es ", os)
+	} else {
+		fmt.Println("Esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s", os)
+	}
 }
